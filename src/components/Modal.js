9db1d8898c2b3d064307842162d28fded3541f67
kiboiWanjohi <sammy313.sw@gmail.com>
2022-07-26 09:31:06 +0300

    enable user authentication

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,25 @@
 import React from "react";
+import { useState } from "react";
+import { app } from "../Firebase/firebaseConfig";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import "./Modal.css";
 function Modal({ closeModal }) {
+  let auth = getAuth();
+  let googleProvider = new GoogleAuthProvider();
+  const [data, setData] = useState({});
+  const handleInput = (event) => {
+    let newInput = { [event.target.name]: event.target.value };
+    setData({ ...data, ...newInput });
+  };
+  const handleSubmit = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((response) => {
+        console.log(response.user);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
   return (
     <div className="modal">
       <div className="modal-container">
@@ -8,14 +27,23 @@ function Modal({ closeModal }) {
           {" "}
           X{" "}
         </button>
-        <input type="email" placeholder="Email" name="email" id="email" />
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          id="email"
+          onChange={(event) => handleInput(event)}
+        />
         <input
           type="text"
           placeholder="Password"
           name="password"
           id="password"
+          onChange={(event) => handleInput(event)}
         />
-        <button className="submit">Submit</button>
+        <button className="submit" onClick={handleSubmit}>
+          Submit
+        </button>
       </div>
     </div>
   );
